0293ff9873c05ffaecfa0cfb004825e887efdbf4
Marco Gómez <marcogonzalo@gmail.com>
2015-08-24 00:24:10 -0430

    cuadro base de informacion de contacto

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(document).ready(function(){
-	$("#header").load("layout/_header.html");
-	$("#footer").load("layout/_footer.html");
+	$('#header').load('layout/_header.html');
+	$('#contact').load('layout/_contact.html');
+	$('#footer').load('layout/_footer.html');
 });
