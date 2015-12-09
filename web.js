bef84d9dba81a006a749350f6837e77765b20640
Emilio Tirado <emilio.tirado57@gmail.com>
2015-12-09 01:25:30 -0430

    test

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 'use strict';
 
-var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
 
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(express.static('dist'));
+app.listen(80);
