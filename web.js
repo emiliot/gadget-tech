133611cd5b157a767b94a401029c165c509de028
Emilio Tirado <emilio.tirado57@gmail.com>
2015-12-09 12:40:17 -0430

    test 5

--- web.js
+++ web.js
@@ -3,12 +3,14 @@ var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.set('port', (process.env.PORT || 80));
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/dist');
+app.set('view engine', 'html');
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 app.get('/', function(request, response){
-	response.sendFile(__dirname + '/dist/index.html');
+	response.render('dist/index.html');
 });
