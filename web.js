c0a3e382cfe735b6a141d42d32fad216106f3be0
Emilio Tirado <emilio.tirado57@gmail.com>
2015-12-09 12:34:54 -0430

    test 4

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
