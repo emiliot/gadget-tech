7f8691e6f066bf8f43be89ea43ce16205e4c6865
Emilio Tirado <emilio.tirado57@gmail.com>
2015-12-09 12:12:59 -0430

    fix

--- web.js
+++ web.js
@@ -8,3 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/', function(request, response){
+	response.render('/dist/index.html');
+});
