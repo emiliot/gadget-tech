1bd2f35a801eb8c69f9bfc4c009a12dcfa329cd2
Emilio Tirado <emilio.tirado57@gmail.com>
2016-01-17 22:30:58 -0430

    sendmail

--- sendMail.php
+++ sendMail.php
@@ -1,3 +1,10 @@
- <?php 
- 	echo '<p>Hello World</p>'; 
- ?>
+<?php
+// El mensaje
+$mensaje = $_POST["first_name"].' '.$_POST['last_name'].' '.$_POST['email'].' '.$_POST['comment'];
+
+// Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
+$mensaje = wordwrap($mensaje, 70, "\r\n");
+
+// Enviarlo
+mail('carlosreyes@gadget-technology.com', 'Contact', $mensaje);
+?>
